8847b34c173faa4f988760c4ca328b337c5e3512
StarKnight <prasenjitt4e@gmail.com>
2024-01-31 20:35:48 +0530

    Learned about props, componenets and functionallity of .jsx files

---
+++ Learning-React.js/first-app/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css";
+function footer() {
+    return (
+        <div className='footer'>
+            Copyright &copy; prasenjit nayak | All rights reserved
+        </div>
+    )
+}
+
+export default footer
