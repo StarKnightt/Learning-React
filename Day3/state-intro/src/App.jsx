db6be2cc6da23772b5d5732e1dbcbd5819c9f6cc
StarKnight <prasenjitt4e@gmail.com>
2024-02-01 21:12:00 +0530

    started learning about React Hooks

---
+++ Day3/state-intro/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // First create a "state" named as "count", then make the initial value "0", then make a function named as "setCount" which helps to update the "count" funciton.
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+      <h1>Learning about hooks</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        {/* <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p> */}
+      </div>
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+      {/* <div>The count is {count}</div>
+      <button onClick={()=>{setCount(count+1)}}>Update the count button</button> */}
+    </>
+  )
+}
+
+export default App
