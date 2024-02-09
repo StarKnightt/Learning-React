bd61534efdaec2926dd625c7307622f2ba7f952e
StarKnight <prasenjitt4e@gmail.com>
2024-02-09 23:25:41 +0530

    started learning handling events in react

---
+++ Day7/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () =>{
+    alert("Hey I'm clicked")
+  }
+
+  return (
+    <>
+      <div className="button">
+        <button onClick={handleClick}>Click me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
