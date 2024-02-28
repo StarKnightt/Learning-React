9efcc0cf100b9b88f62e3f83f582e0557395a413
StarKnight <prasenjitt4e@gmail.com>
2024-02-28 23:05:01 +0530

    started learning about useMemo Hook in react

---
+++ Day10/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const nums = new Array(30_000_000).fill(0).map((_, i)=>{
+  return {
+    index: i,
+    isMagical: i===29_000_0
+  }
+})
+
+// 4:38
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [numbers, setnumbers] = useState(nums)
+ 
+   const magical = numbers.find(item=>item.isMagical==true)
+
+  return (
+    <>
+      <div>
+        <span>Magical number is {magical.i}</span>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
