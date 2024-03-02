9c19a39a288731a06585e597f21316733e35ec50
StarKnight <prasenjitt4e@gmail.com>
2024-03-02 23:08:03 +0530

    learned about useMemo in react

--- Day10/src/App.jsx
+++ Day10/src/App.jsx
@@ -1,48 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useMemo } from "react";
+import "./App.css";
 
-const nums = new Array(30_000_000).fill(0).map((_, i)=>{
+const nums = new Array(30_000_000).fill(0).map((_, i) => {
   return {
     index: i,
-    isMagical: i===29_000_0
-  }
-})
-
-// 4:38
+    isMagical: i === 29_000_000,
+  };
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [numbers, setnumbers] = useState(nums)
-  
-   const magical = numbers.find(item=>item.isMagical==true)
+  const [count, setCount] = useState(0);
+  const [numbers, setNumbers] = useState(nums);
+
+  // const magical = numbers.find(item=>item.isMagical===true);  // expensive computation
+  const magical = useMemo(() => numbers.find((item) => item.isMagical === true), [numbers]);
 
   return (
     <>
       <div>
-        <span>Magical number is {magical.i}</span>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>Magical number is {magical.index}</span>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+            if (count === 10) {
+              setNumbers(new Array(10_000_000).fill(0).map((_, i) => {
+                return {
+                  index: i,
+                  isMagical: i === 9_000_000,
+                };
+              }));
+            }
+          }}
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
