0755a2fd60652c8aa764870867316b8233df38a7
StarKnight <prasenjitt4e@gmail.com>
2024-03-03 22:58:57 +0530

    started learning callback hook

---
+++ Day11/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar =({adjective}) => {
+    console.log("Navbar is rendered")
+  return (
+    <div>
+      I am a {adjective} Navbar
+    </div>
+  )
+}
+
+export default Navbar
