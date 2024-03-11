af7fcb0bd6450594dab0fe51becfd3f8fe6441ca
StarKnight <prasenjitt4e@gmail.com>
2024-03-11 19:17:22 +0530

    removed unnecssary text from the form

--- FormHandling/src/App.jsx
+++ FormHandling/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import { useForm } from "react-hook-form";
 
 function App() {
   const {
     register,
     handleSubmit,
-    setError,    
+    setError,
     formState: { errors, isSubmitting },
   } = useForm();
 
@@ -23,9 +23,9 @@ function App() {
     let r = await fetch("http://localhost:3000/", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json", 
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
     let res = await r.text();
     console.log(data, res);
@@ -38,26 +38,41 @@ function App() {
   };
 
   return (
-    <> 
-      {isSubmitting && <div>Loading...</div>}    
+    <>
+      {isSubmitting && <div>Loading...</div>}
       Validation Form
       <div className="container">
         <form action="" onSubmit={handleSubmit(onSubmit)}>
-          <input placeholder='username' {...register("username", { 
-            required: { value: true, message: "This field is required" }, 
-            minLength: { value: 3, message: "Min length is 3" }, 
-            maxLength: { value: 10, message: "Max length is 10" } 
-          })} type="text"   />
-          {errors.username && <div className='red'>{errors.username.message}</div>}
+          <input
+            placeholder="username"
+            {...register("username", {
+              required: { value: true, message: "This field is required" },
+              minLength: { value: 3, message: "Min length is 3" },
+              maxLength: { value: 10, message: "Max length is 10" },
+            })}
+            type="text"
+          />
+          {errors.username && (
+            <div className="red">{errors.username.message}</div>
+          )}
           <br />
-          <input placeholder='password'  {...register("password", {
-            minLength: { value: 7, message: "Min length of password is 7" }
-          })} type="password"/>
-          {errors.password && <div className='red'>{errors.password.message}</div>}k
+          <input
+            placeholder="password"
+            {...register("password", {
+              minLength: { value: 7, message: "Min length of password is 7" },
+            })}
+            type="password"
+          />
+          {errors.password && (
+            <div className="red">{errors.password.message}</div>
+          )}
+          
           <br />
           <input disabled={isSubmitting} type="submit" value="Submit" />
-          {errors.myform && <div className='red'>{errors.myform.message}</div>}
-          {errors.blocked && <div className='red'>{errors.blocked.message}</div>}
+          {errors.myform && <div className="red">{errors.myform.message}</div>}
+          {errors.blocked && (
+            <div className="red">{errors.blocked.message}</div>
+          )}
         </form>
       </div>
     </>
