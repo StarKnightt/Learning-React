172e752c6703a9b5a3bf791d23d5bdb5ecf1ca48
StarKnight <prasenjitt4e@gmail.com>
2024-03-25 22:24:38 +0530

    will be learning everything from tomorrow I hope

--- Day12/src/App.jsx
+++ Day12/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0) 
+  const [count, setCount] = useState(0);
   return (
     <>
       <div>
@@ -28,9 +28,6 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-} 
-// I hope everything will be okay as soon as possible, why it's taking so much time for house repairing :(
-//  will be learning Redux as soon as possible bro, can't getting time because of this house repairing
-export default App
-// 2:26 
+  );
+}
+export default App;
