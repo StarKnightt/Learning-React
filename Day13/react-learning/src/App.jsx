f212bf4e0cc896630c1308a981c35cfcd25936d3
StarKnightt <prasenjitt4e@gmail.com>
2024-06-27 21:02:10 +0530

    not much coding today

--- Day13/react-learning/src/App.jsx
+++ Day13/react-learning/src/App.jsx
@@ -1,36 +1,44 @@
-import { useState } from 'react'
 import React from 'react'
 import './App.css'
 
-const Person = () =>{
-  return(
+const Person = () => {
+  const FirstName = "John";
+  const SecondName = "Doe"
+  // const firstname = "3";
+  // const lastname = "4";
+  // const fullname = parseInt(firstname) + parseInt(lastname);
+  return (
     <>
-    <h1 className='hey'>Name: John </h1>
-    <h2>Last name: Doe</h2>
-    <h2>Age: 30</h2>
+      <h1 className='hey'>Name: John </h1>
+      <h2>Last name: Doe</h2>
+      <h2>Age: 30</h2>
+      <h1 className='hey'>Name:{(FirstName)} {(SecondName)}</h1>
+      {/* <h2 className='output'>Name:{fullname}</h2> */}
     </>
   )
 }
-
-const App = () =>{
+const App = () => {
   return (
     <div className="App">
-     <Person />
-      <h1>Hello {2+2}</h1>
+      <Person />
+      <Person />
+      <Person />
+
+      <h1>Hello {2 + 2}</h1>
     </div>
   );
 }
 
 // const App = () => {
-  // const name = 'prasen'; // null = those two line of code, '' = name as per you use.
-  // const isNameShowing = true;
-  // return (
-  //   <>
-      {/* <div> */}
-      {/* <h1>hello world</h1> */}
-        {/* <h1>Hello {isNameShowing ? name: 'Someone'}</h1> */}
-        {/* <h2>The answer is {math} </h2> */}
-        {/* {name ? ( 
+// const name = 'prasen'; // null = those two line of code, '' = name as per you use.
+// const isNameShowing = true;
+// return (
+//   <>
+{/* <div> */ }
+{/* <h1>hello world</h1> */ }
+{/* <h1>Hello {isNameShowing ? name: 'Someone'}</h1> */ }
+{/* <h2>The answer is {math} </h2> */ }
+{/* {name ? ( 
           <>
           <h1>{name}</h1>
         </>
