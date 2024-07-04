502b1a58fb25b703e6b2db65fd9454476d213de4
StarKnightt <prasenjitt4e@gmail.com>
2024-07-04 22:30:37 +0530

    formatted the document lol

--- Day14/day14/src/App.jsx
+++ Day14/day14/src/App.jsx
@@ -1,18 +1,23 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const[counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  useEffect(()=>{
-   alert("You've changed the counter to " + counter)
-  },[counter]);
+  useEffect(() => {
+    //  alert("You've changed the counter to " + counter)
+  }, [counter]);
 
   return (
     <div className="App">
-      <button onClick={()=>setCounter((prevCount)=>prevCount -1)}>-</button>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
       <h1>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=>prevCount +1)}>+</button>
+      <h1> hey world</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
 }
